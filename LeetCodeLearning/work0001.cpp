afad42ae516237351d03e5bbb539bf8c5e5996f9
Lzcdebear <luzhichen18117565581@gmail.com>
2023-09-18 19:26:02 +0800

    leetcode

---
+++ LeetCodeLearning/work0001.cpp
@@ -0,0 +1,14 @@
+//无论心情怎么样，今天也要继续加油学习哦
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int nums[4] = {0};
+    while 
+    system("pause");
+    return 0;
+}
