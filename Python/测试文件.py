a3f04003a9104c4aec9e89ff49a11a2f1fc965e3
Lzcdebear <luzhichen18117565581@gmail.com>
2023-11-06 00:16:28 +0800

    music classcifier

--- Python/测试文件.py
+++ Python/测试文件.py
@@ -1,11 +1,1 @@
-class TellWhat():
-    def __init__(self):
-        self.name = ''
-    
-    def TellTheFood(self,name):
-        self.name = '鸭脖'
-        print('这个食物是',self.name)
-
-food = input('这个食物是什么')
-tellwhat = TellWhat()
-tellwhat.TellTheFood(food)
+import pkuseg
