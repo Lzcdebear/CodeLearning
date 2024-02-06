63ace82a12f34fc301ddbc4848a9fe4c045b2f94
Lzcdebear <luzhichen18117565581@gmail.com>
2024-02-06 13:06:38 +0800

    changefolder

---
+++ CPP/shayebushi.cpp
@@ -0,0 +1,13 @@
+//无论心情怎么样，今天也要继续加油学习哦
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+    printf("你到底是什么东西啊，为什么事情这么多你还有空看这个的啊\t好好反思一下自己是不是太闲了\t");
+    system("pause");
+    return 0;
+}
